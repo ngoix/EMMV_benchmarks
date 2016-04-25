53424b5697be14f0e4236fe76b6483bc450b559f
ngoix <goix.nicolas@gmail.com>
2016-04-26 01:28:37 +0200

    factorize data loading + rm discont. features

--- em_bench.py
+++ em_bench.py
@@ -13,20 +13,17 @@ from matplotlib import pyplot as plt
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.svm import OneClassSVM
 from sklearn.ensemble import IsolationForest
-from sklearn.utils import shuffle as sh
-from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_mldata
-from sklearn.datasets import fetch_spambase, fetch_annthyroid, fetch_arrhythmia
-from sklearn.datasets import fetch_pendigits, fetch_pima, fetch_wilt
-from sklearn.datasets import fetch_internet_ads, fetch_adult
+
+from sklearn.datasets import one_class_data
+
 from em import em, mv  # , EM_approx, MV_approx, MV_approx_over
-from sklearn.preprocessing import LabelBinarizer, scale
 
 n_generated = 100000
 alpha_min = 0.9
 alpha_max = 0.999
 t_max = 0.9
 ocsvm_max_train = 50000
-#wilt: prendre t_max = 0.995, alpha_min = 0.995, alpha_max=0.999 (sans scale)
+# wilt: prendre t_max = 0.995, alpha_min = 0.995, alpha_max=0.999 (sans scale)
 np.random.seed(1)
 
 # TODO: find good default parameters for every datasets
@@ -56,140 +53,13 @@ for dat in datasets:
     plt.clf()
     plt.figure(figsize=(25, 13))
 
-    print 'dataset:', dat
     # loading and vectorization
-    print('loading data')
-
-    if dat == 'adult':
-        dataset = fetch_adult(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        # anormal data are those with label >50K:
-        y = np.all((y != ' <=50K', y != ' <=50K.'), axis=0).astype(int)
-
-    if dat == 'internet_ads':  # not adapted to oneclassrf
-        dataset = fetch_internet_ads(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y == 'ad.').astype(int)
-
-    if dat == 'wilt':
-        dataset = fetch_wilt(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y == 'w').astype(int)
-
-    if dat == 'pima':
-        dataset = fetch_pima(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-
-    if dat == 'pendigits':
-        dataset = fetch_pendigits(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y == 4).astype(int)
-        # anomalies = class 4
-
-    if dat == 'arrhythmia':
-        dataset = fetch_arrhythmia(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        # rm 5 features containing some '?' (XXX to be mentionned in paper)
-        X = np.delete(X, [10, 11, 12, 13, 14], axis=1)
-        y = (y != 1).astype(int)
-        # normal data are then those of class 1
-
-    if dat == 'annthyroid':
-        dataset = fetch_annthyroid(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y != 3).astype(int)
-        # normal data are then those of class 3
-
-    if dat == 'spambase':
-        dataset = fetch_spambase(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-
-    if dat == 'ionosphere':
-        dataset = fetch_mldata('ionosphere')
-        X = dataset.data
-        y = dataset.target
-        X, y = sh(X, y)
-        y = (y != 1).astype(int)
-
-    if dat in ['http', 'smtp', 'SA', 'SF']:
-        dataset = fetch_kddcup99(subset=dat, shuffle=True, percent10=False)
-        X = dataset.data
-        y = dataset.target
-
-    if dat == 'shuttle':
-        dataset = fetch_mldata('shuttle')
-        X = dataset.data
-        y = dataset.target
-        X, y = sh(X, y)
-        # we remove data with label 4
-        # normal data are then those of class 1
-        s = (y != 4)
-        X = X[s, :]
-        y = y[s]
-        y = (y != 1).astype(int)
-
-    if dat == 'forestcover':
-        dataset = fetch_covtype(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        # normal data are those with attribute 2
-        # abnormal those with attribute 4
-        s = (y == 2) + (y == 4)
-        X = X[s, :]
-        y = y[s]
-        y = (y != 2).astype(int)
-
-    print('vectorizing data')
-
-    if dat == 'SF':
-        lb = LabelBinarizer()
-        lb.fit(X[:, 1])
-        x1 = lb.transform(X[:, 1])
-        X = np.c_[X[:, :1], x1, X[:, 2:]]
-        y = (y != 'normal.').astype(int)
-
-    if dat == 'SA':
-        lb = LabelBinarizer()
-        lb.fit(X[:, 1])
-        x1 = lb.transform(X[:, 1])
-        lb.fit(X[:, 2])
-        x2 = lb.transform(X[:, 2])
-        lb.fit(X[:, 3])
-        x3 = lb.transform(X[:, 3])
-        X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
-        y = (y != 'normal.').astype(int)
-
-    if dat == 'http' or dat == 'smtp':
-        y = (y != 'normal.').astype(int)
-
-    # ### 10 % of abnormal max: ###
-    index_normal = (y == 0)
-    index_abnormal = (y == 1)
-    if index_abnormal.sum() > 0.1 * index_normal.sum():
-        X_normal = X[index_normal]
-        X_abnormal = X[index_abnormal]
-        n_anomalies = X_abnormal.shape[0]
-        n_anomalies_max = int(0.1 * index_normal.sum())
-        r = sh(np.arange(n_anomalies))[:n_anomalies_max]
-        X = np.r_[X_normal, X_abnormal[r]]
-        y = np.array([0] * X_normal.shape[0] + [1] * n_anomalies_max)
-    # ######
+    X, y = one_class_data(dat)
 
     n_samples, n_features = np.shape(X)
     n_samples_train = n_samples // 2
     n_samples_test = n_samples - n_samples_train
 
-    X = X.astype(float)
-    # if X.std() > 1:
-    X = scale(X)
     X_train = X[:n_samples_train, :]
     X_test = X[n_samples_train:, :]
     y_train = y[:n_samples_train]
--- em_bench_high.py
+++ em_bench_high.py
@@ -8,23 +8,19 @@ sys.path.insert(1, '/home/nicolas/Bureau/OCRF')
 
 import matplotlib
 matplotlib.use('Agg')
-from matplotlib import pyplot as plt
 
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.svm import OneClassSVM
 from sklearn.ensemble import IsolationForest
 from sklearn.utils import shuffle as sh
-from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_mldata
-from sklearn.datasets import fetch_spambase, fetch_annthyroid, fetch_arrhythmia
-from sklearn.datasets import fetch_pendigits, fetch_pima, fetch_wilt
-from sklearn.datasets import fetch_internet_ads, fetch_adult
+from sklearn.datasets import one_class_data
+
 from em import em, mv  # , EM_approx, MV_approx, MV_approx_over
-from sklearn.preprocessing import LabelBinarizer, scale
-from sklearn.utils import shuffle
+
 
 averaging = 20
 max_features = 3
-n_generated = 500000
+n_generated = 100000
 alpha_max = 0.9
 t_max = 0.9
 ocsvm_max_train = 10000
@@ -52,143 +48,17 @@ np.random.seed(1)
 # datasets = [# 'http',
 #             'smtp', 'shuttle', # 'spambase',
 #             'pendigits', 'pima', 'wilt', 'adult']
-datasets = ['ionosphere', 'spambase', 'annthyroid', 'arrhythmia', 'forestcover']
+
+# datasets = ['ionosphere', 'spambase', 'annthyroid', 'arrhythmia',
+#             'forestcover']
+datasets = ['arrhythmia']
 
 for dat in datasets:
-
-    print 'dataset:', dat
     # loading and vectorization
-    print('loading data')
-
-    if dat == 'adult':
-        dataset = fetch_adult(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        # anormal data are those with label >50K:
-        y = np.all((y != ' <=50K', y != ' <=50K.'), axis=0).astype(int)
-
-    if dat == 'internet_ads':  # not adapted to oneclassrf
-        dataset = fetch_internet_ads(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y == 'ad.').astype(int)
-
-    if dat == 'wilt':
-        dataset = fetch_wilt(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y == 'w').astype(int)
-
-    if dat == 'pima':
-        dataset = fetch_pima(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-
-    if dat == 'pendigits':
-        dataset = fetch_pendigits(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y == 4).astype(int)
-        # anomalies = class 4
-
-    if dat == 'arrhythmia':
-        dataset = fetch_arrhythmia(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        # rm 5 features containing some '?' (XXX to be mentionned in paper)
-        X = np.delete(X, [10, 11, 12, 13, 14], axis=1)
-        y = (y != 1).astype(int)
-        # normal data are then those of class 1
-
-    if dat == 'annthyroid':
-        dataset = fetch_annthyroid(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        y = (y != 3).astype(int)
-        # normal data are then those of class 3
-
-    if dat == 'spambase':
-        dataset = fetch_spambase(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-
-    if dat == 'ionosphere':
-        dataset = fetch_mldata('ionosphere')
-        X = dataset.data
-        y = dataset.target
-        X, y = sh(X, y)
-        y = (y != 1).astype(int)
-
-    if dat in ['http', 'smtp', 'SA', 'SF']:
-        dataset = fetch_kddcup99(subset=dat, shuffle=True, percent10=False)
-        X = dataset.data
-        y = dataset.target
-
-    if dat == 'shuttle':
-        dataset = fetch_mldata('shuttle')
-        X = dataset.data
-        y = dataset.target
-        X, y = sh(X, y)
-        # we remove data with label 4
-        # normal data are then those of class 1
-        s = (y != 4)
-        X = X[s, :]
-        y = y[s]
-        y = (y != 1).astype(int)
-
-    if dat == 'forestcover':
-        dataset = fetch_covtype(shuffle=True)
-        X = dataset.data
-        y = dataset.target
-        # normal data are those with attribute 2
-        # abnormal those with attribute 4
-        s = (y == 2) + (y == 4)
-        X = X[s, :]
-        y = y[s]
-        y = (y != 2).astype(int)
-
-    print('vectorizing data')
-
-    if dat == 'SF':
-        lb = LabelBinarizer()
-        lb.fit(X[:, 1])
-        x1 = lb.transform(X[:, 1])
-        X = np.c_[X[:, :1], x1, X[:, 2:]]
-        y = (y != 'normal.').astype(int)
-
-    if dat == 'SA':
-        lb = LabelBinarizer()
-        lb.fit(X[:, 1])
-        x1 = lb.transform(X[:, 1])
-        lb.fit(X[:, 2])
-        x2 = lb.transform(X[:, 2])
-        lb.fit(X[:, 3])
-        x3 = lb.transform(X[:, 3])
-        X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
-        y = (y != 'normal.').astype(int)
-
-    if dat == 'http' or dat == 'smtp':
-        y = (y != 'normal.').astype(int)
-
-    # ### 10 % of abnormal max: ###
-    index_normal = (y == 0)
-    index_abnormal = (y == 1)
-    if index_abnormal.sum() > 0.1 * index_normal.sum():
-        X_normal = X[index_normal]
-        X_abnormal = X[index_abnormal]
-        n_anomalies = X_abnormal.shape[0]
-        n_anomalies_max = int(0.1 * index_normal.sum())
-        r = sh(np.arange(n_anomalies))[:n_anomalies_max]
-        X = np.r_[X_normal, X_abnormal[r]]
-        y = np.array([0] * X_normal.shape[0] + [1] * n_anomalies_max)
-    # ######
-
+    X, y = one_class_data(dat)
     n_samples, n_features = np.shape(X)
     n_samples_train = n_samples // 2
     n_samples_test = n_samples - n_samples_train
-
-    X = X.astype(float)
-    X = scale(X)
 
     X_train = X[:n_samples_train, :]
     X_test = X[n_samples_train:, :]
@@ -210,44 +80,46 @@ for dat in datasets:
     em_iforest, mv_iforest = 0, 0
     em_lof, mv_lof = 0, 0
     em_ocsvm, mv_ocsvm = 0, 0
-
-    for nb_exp in range(averaging):
-        features = shuffle(np.arange(n_features))[:max_features]
+    nb_exp = 0
+    while nb_exp < averaging:
+        features = sh(np.arange(n_features))[:max_features]
         X_train_ = X_train[:, features]
         X_ = X_test[:, features]
 
         lim_inf = X_.min(axis=0)
         lim_sup = X_.max(axis=0)
         volume_support = (lim_sup - lim_inf).prod()
-        t = np.arange(0, 100 / volume_support, 0.001 / volume_support)
-        axis_alpha = np.arange(alpha_max, 0.99, 0.001)
-        unif = np.random.uniform(lim_inf, lim_sup,
-                                 size=(n_generated, max_features))
+        if volume_support > 0:
+            nb_exp += 1
+            t = np.arange(0, 100 / volume_support, 0.001 / volume_support)
+            axis_alpha = np.arange(alpha_max, 0.99, 0.001)
+            unif = np.random.uniform(lim_inf, lim_sup,
+                                     size=(n_generated, max_features))
 
-        iforest.fit(X_train_)
-        lof.fit(X_train_)
-        ocsvm.fit(X_train_[:min(ocsvm_max_train, n_samples_train - 1)])
-        print 'end of ocsvm training!'
-        s_X_iforest = iforest.decision_function(X_)
-        s_X_lof = lof.decision_function(X_)
-        s_X_ocsvm = ocsvm.decision_function(X_).reshape(1, -1)[0]
+            iforest.fit(X_train_)
+            lof.fit(X_train_)
+            ocsvm.fit(X_train_[:min(ocsvm_max_train, n_samples_train - 1)])
+            print 'end of ocsvm training!'
+            s_X_iforest = iforest.decision_function(X_)
+            s_X_lof = lof.decision_function(X_)
+            s_X_ocsvm = ocsvm.decision_function(X_).reshape(1, -1)[0]
 
-        s_unif_iforest = iforest.decision_function(unif)
-        s_unif_lof = lof.decision_function(unif)
-        s_unif_ocsvm = ocsvm.decision_function(unif).reshape(1, -1)[0]
+            s_unif_iforest = iforest.decision_function(unif)
+            s_unif_lof = lof.decision_function(unif)
+            s_unif_ocsvm = ocsvm.decision_function(unif).reshape(1, -1)[0]
 
-        em_iforest += em(t, t_max, volume_support, s_unif_iforest,
-                         s_X_iforest, n_generated)[0]
-        mv_iforest += mv(axis_alpha, volume_support, s_unif_iforest,
-                         s_X_iforest, n_generated)[0]
-        em_lof += em(t, t_max, volume_support, s_unif_lof, s_X_lof,
-                     n_generated)[0]
-        mv_lof += mv(axis_alpha, volume_support, s_unif_lof,
-                     s_X_lof, n_generated)[0]
-        em_ocsvm += em(t, t_max, volume_support, s_unif_ocsvm,
-                       s_X_ocsvm, n_generated)[0]
-        mv_ocsvm += mv(axis_alpha, volume_support, s_unif_ocsvm,
-                       s_X_ocsvm, n_generated)[0]
+            em_iforest += em(t, t_max, volume_support, s_unif_iforest,
+                             s_X_iforest, n_generated)[0]
+            mv_iforest += mv(axis_alpha, volume_support, s_unif_iforest,
+                             s_X_iforest, n_generated)[0]
+            em_lof += em(t, t_max, volume_support, s_unif_lof, s_X_lof,
+                         n_generated)[0]
+            mv_lof += mv(axis_alpha, volume_support, s_unif_lof,
+                         s_X_lof, n_generated)[0]
+            em_ocsvm += em(t, t_max, volume_support, s_unif_ocsvm,
+                           s_X_ocsvm, n_generated)[0]
+            mv_ocsvm += mv(axis_alpha, volume_support, s_unif_ocsvm,
+                           s_X_ocsvm, n_generated)[0]
 
     em_iforest /= averaging
     mv_iforest /= averaging
